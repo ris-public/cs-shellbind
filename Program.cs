6ea35e0cc1796a98716fa1696c780191a8ca322e
RishiKL <52776758+ris-public@users.noreply.github.com>
2019-09-06 10:59:25 +0530

    ProxyClient
    ProxyClient

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
 
-namespace cs_shellbind
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
+namespace ProxyClient{
+		class Program
+		{
+				static void Main(string[] args)
+				{
+						Console.WriteLine("Hello World!");
+						DTLSServer dtls = new DTLSServer("127.0.0.1", "10000", new byte[] {0xBA,0xA0});
+						dtls.Unbuffer="stdbuf";
+						dtls.Unbuffer_Args="-i0 -o0";
+						dtls.Start();
+						dtls.GetStream().Write(Encoding.Default.GetBytes("It's Working!"+Environment.NewLine));
+						dtls.WaitForExit();
+				}
+		}
 }
