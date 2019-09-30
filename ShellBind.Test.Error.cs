c31ed9c0f65ff2ea7e40c30d8924b03e00024453
RishiKL <52776758+ris-public@users.noreply.github.com>
2019-10-01 02:53:01 +0530

    v1.1.2.

--- ShellBind.Test.Error.cs
+++ ShellBind.Test.Error.cs
@@ -2,13 +2,13 @@
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 
 
 using System;
@@ -24,30 +24,31 @@ using static System.Runtime.InteropServices.RuntimeInformation;
 
 namespace ProxyClient
 {
-        class Program_Error
-        {
-                static int Main(string[] args){
+		class Program_Error
+		{
+				static int Main(string[] args){
+						ShellSocket SS;
 #if NETCOREAPP3_0
 						if ( IsOSPlatform(OSPlatform.Linux) ||  IsOSPlatform(OSPlatform.FreeBSD) ||  IsOSPlatform(OSPlatform.OSX))
 #else
-						if ( IsOSPlatform(OSPlatform.Linux) ||  IsOSPlatform(OSPlatform.OSX))
+								if ( IsOSPlatform(OSPlatform.Linux) ||  IsOSPlatform(OSPlatform.OSX))
 #endif
-                        ShellSocket SS = new ShellSocket("bash -c \"yes hi 1>&2\"", "hi");
-						else
-                        ShellSocket SS = new ShellSocket("cmd /c \"echo hi 1>&2\"", "");
-                        System.Console.WriteLine("Starting...");
+										SS = new ShellSocket("bash -c \"yes hi 1>&2\"", "hi");
+								else
+										SS = new ShellSocket("cmd /c \"echo hi 1>&2\"", "");
+						System.Console.WriteLine("Starting...");
 						SS.RedirectErrorsToConsole=true;
-                        SS.Start();
-                        Stream S = SS.GetStream();
-                        System.Console.WriteLine("ShellSocket created...");
-                        pair P = new pair(new StreamReader(Console.OpenStandardInput()), new StreamWriter(Console.OpenStandardOutput()));
-                        System.Console.WriteLine("Pair created...");
-                        System.Console.WriteLine("{0}", new StreamWriter(Console.OpenStandardOutput()));
-                        System.Console.WriteLine("Binding...");
-                        System.Console.WriteLine("P:{0} S:{1}", P, S);
-                        Rishi.PairStream.pair.BindStreams(P, S);
-                        return 0;
-                }
-        }
+						SS.Start();
+						Stream S = SS.GetStream();
+						System.Console.WriteLine("ShellSocket created...");
+						pair P = new pair(new StreamReader(Console.OpenStandardInput()), new StreamWriter(Console.OpenStandardOutput()));
+						System.Console.WriteLine("Pair created...");
+						System.Console.WriteLine("{0}", new StreamWriter(Console.OpenStandardOutput()));
+						System.Console.WriteLine("Binding...");
+						System.Console.WriteLine("P:{0} S:{1}", P, S);
+						Rishi.PairStream.pair.BindStreams(P, S);
+						return 0;
+				}
+		}
 }
 
