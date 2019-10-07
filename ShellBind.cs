28e365dcbef3fd76a6a608a86351651da9eb80c8
RishiKL <52776758+ris-public@users.noreply.github.com>
2019-10-07 06:26:43 +0530

    v1.1.6.

--- ShellBind.cs
+++ ShellBind.cs
@@ -335,7 +335,7 @@ namespace Rishi.ShellBind {
 				{
 						if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 						{
-								return Environment.GetEnvironmentVariable("PATH").Split(";");
+								return Environment.GetEnvironmentVariable("PATH").Split(';');
 						}
 #if !(NETSTANDARD2_0 || NETCOREAPP2_0 || NETCOREAPP2_1 || NETCOREAPP2_2)
 						else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)|| RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
@@ -343,7 +343,7 @@ namespace Rishi.ShellBind {
 						else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
 #endif
 						{
-								return Environment.GetEnvironmentVariable("PATH").Split(";");
+								return Environment.GetEnvironmentVariable("PATH").Split(':');
 						}
 						else return new string[] { Directory.GetCurrentDirectory() };
 				}
